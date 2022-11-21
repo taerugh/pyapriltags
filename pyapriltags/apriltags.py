18a6bcde9cee6945b9189f52127a9ca47ac8f0cf
WillB97 <will@rudi.org.uk>
2022-11-21 19:32:21 +0000

    Fix most linting and static-typing errors

--- pyapriltags/apriltags.py
+++ pyapriltags/apriltags.py
@@ -15,7 +15,7 @@ Current maintainer: Will Barber
 import ctypes
 import os
 import sys
-from typing import Dict, List, Union, Optional, NamedTuple
+from typing import Any, Dict, List, Union, Optional, NamedTuple
 
 import numpy
 
@@ -207,17 +207,17 @@ class Detector(object):
     """
 
     def __init__(self,
-                 families: str='tag36h11',
-                 nthreads: int=1,
-                 quad_decimate: float=2.0,
-                 quad_sigma: float=0.0,
-                 refine_edges: int=1,
-                 decode_sharpening: float=0.25,
-                 debug: int=0,
-                 searchpath: List[str]=['apriltags', '.', dir_path]):
+                 families: str = 'tag36h11',
+                 nthreads: int = 1,
+                 quad_decimate: float = 2.0,
+                 quad_sigma: float = 0.0,
+                 refine_edges: int = 1,
+                 decode_sharpening: float = 0.25,
+                 debug: int = 0,
+                 searchpath: List[str] = ['apriltags', '.', dir_path]):
 
         # Parse the parameters
-        self.params = dict()
+        self.params: Dict[str, Any] = dict()
         self.params['families'] = families.split()
         self.params['nthreads'] = nthreads
         self.params['quad_decimate'] = quad_decimate
@@ -350,8 +350,8 @@ class Detector(object):
                     self.libc.tagStandard52h13_destroy(tf)
 
     def detect(
-        self, img: numpy.ndarray, estimate_tag_pose: bool=False,
-        camera_params: Optional[numpy.ndarray]=None, tag_size: Union[float, None, Dict[int, float]]=None,
+        self, img: numpy.ndarray, estimate_tag_pose: bool = False,
+        camera_params: Optional[numpy.ndarray] = None, tag_size: Union[float, None, Dict[int, float]] = None,
     ) -> List[Detection]:
         """
         Run detectons on the provided image. The image must be a grayscale
@@ -363,6 +363,9 @@ class Detector(object):
         c_img = self._convert_image(img)
 
         return_info = []
+
+        if self.libc is None:
+            raise RuntimeError('No DLL found')
 
         # detect apriltags in the image
         self.libc.apriltag_detector_detect.restype = ctypes.POINTER(_ZArray)
